7b6fa18b94e88c0fd1eb8a6f0c421630c38847e0
luke0004 <lukas.spieker@gmail.com>
2026-02-01 10:21:30 +0100

    Adds YouTube video thumbnail support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import mdx from '@astrojs/mdx';
 
 export default defineConfig({
   site: 'https://coelacanth.ch',   // WICHTIG für korrekte URLs
-  integrations: [sitemap()],
+  integrations: [sitemap(), mdx()],
   output: 'static'
 });
